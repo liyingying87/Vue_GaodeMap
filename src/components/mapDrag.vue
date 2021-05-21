aa6f8c9a721cf7ac6fdfbc040afb1cabf3733df5
Ann <1418845580@qq.com>
2021-05-21 21:40:28 +0800

    实现了车2D轨迹运行

--- src/components/mapDrag.vue
+++ src/components/mapDrag.vue
@@ -2,11 +2,20 @@
 <template>
   <div class="m-map">
     <div id="container" class="map">正在加载数据 ...</div>
-    <div v-for="lnglat in lngLats" v-bind:key="lnglat.id">
-     {{lnglat.id}}
-      {{lnglat.x}}
-      {{lnglat.y}}
-      <p></p>
+<!--    //选择路线-->
+    <div id="choose_line">
+      <select v-model="selected" @change="selectTrigger(selected)">
+        <option  value=" " disabled>请选择路线</option>
+        <option v-for ="item in lines"
+        :key = "item.id"
+        :label="item.name+'途径的站点有：'+item.stations"
+        :value="item.id"></option>
+      </select>
+      <span>Selected: {{ selected }}</span>
+      <!--//清空路线-->
+      <button name="clear">
+      清空路线
+    </button>
     </div>
   </div>
 </template>
@@ -14,56 +23,52 @@
 import remoteLoad from '@/utils/remoteLoad.js'
 import {MapKey} from '@/config/config'
 import { mapState } from 'vuex'
-import {store} from '../store/index.js'
-import {LabelsData} from '../Data/Markers_locations'
 
 export default {
   name:'mapDrag',
   data() {
-    return {}
+    return {
+      selected:'',
+      AMapUI: null,
+      AMap: null,
+      mapObj:null,
+      car_marker:null
+    }
   },
   computed:{
-    ...mapState(['lngLats'])
+    ...mapState(['lngLats','lines'])
   },
   methods: {
     // 实例化地图
     initMap() {
-      console.log(this.lngLats.length+"..");
-      const test1 = 4;
-      //这里怎么存进去是个问题
       const lnglat_my =this.lngLats ;
-      console.log("类型是："+typeof this.lngLats);
-      console.log(this.lngLats);
-      //
-      // for (let i=0,len = this.lngLats.length;i<len;i++){
-      //   lnglat_my[i]=8;
-      // }
-      console.log("数组大小为："+lnglat_my);
-      console.log(test1+"vvvv..");
+
       let AMapUI = this.AMapUI = window.AMapUI;
       let AMap = this.AMap = window.AMap;
-      console.log("-");
-      let map = new AMap.Map('container', {
+
+      this.mapObj = new AMap.Map('container', {
         zoom: 16,
         center: [121.504374,31.030502]
       });
-      console.log("--");
+      console.log(this.mapObj+",,");
+      console.log(this.mapObj.getCenter());
+      // let marker = new AMap.Marker({
+      //   map: mapObj,
+      //   position: this.lines.find(item => item.id == 1).lineArr[0],
+      //   icon: "https://webapi.amap.com/images/car.png",
+      //   offset: new AMap.Pixel(-26, -13),
+      //   autoRotation: true,
+      //   angle:-90,
+      // });
       //异步执行函数 参考此网址https://lbs.amap.com/api/amap-ui/intro
+      const mapObj_my = this.mapObj;
       AMapUI.loadUI(['overlay/SimpleMarker'], function (SimpleMarker) {
-        console.log("---");
-        console.log(test1+"vvvvv");
-        console.log("数组大小为："+lnglat_my.length);
-        lnglat_my.forEach((item) => {
-          console.log(item);
-        })
-        //console.log(this.lngLats.length+"./");
-
+        //学会此种书写方式
+        // lnglat_my.forEach((item) => {
+        //   console.log(item);
+        // })
         let awIcons = 'address-book';
-        let colors = "#d62728";
-        console.log("----");
-      //  console.log(this.lngLats.length);
         for (let i = 0, len = lnglat_my.length; i < len; i++) {
-          console.log("-----");
           new SimpleMarker({
             //设置awesomeIcon
             awesomeIcon: awIcons,
@@ -79,28 +84,64 @@ export default {
               }
             },
             iconStyle: 'red',
-            map: map,
+            map: mapObj_my,
             position: [lnglat_my[i].x,lnglat_my[i].y]
           });
-          console.log("------");
-          //console.log([this.lngLats[i].x,this.lngLats[i].y]);
         }
       });
+    },
+    //根据获得的路线的id值，进行画路线
+    selectTrigger(val){
+      let AMap = this.AMap = window.AMap;
+      this.car_marker = new AMap.Marker({
+        map: this.mapObj,
+        position: this.lines.find(item => item.id == val).lineArr[0],
+        icon: "https://webapi.amap.com/images/car.png",
+        offset: new AMap.Pixel(-26, -13),
+        autoRotation: true,
+        angle:-90,
+      });//画出来小车
+
+      let polyline = new AMap.Polyline({
+        map: this.mapObj,
+        path: this.lines.find(item => item.id == val).lineArr,
+        showDir:true,
+        strokeColor: "#28F",  //线颜色
+        // strokeOpacity: 1,     //线透明度
+        strokeWeight: 6,      //线宽
+        // strokeStyle: "solid"  //线样式
+      });//画出来地上的线
+
+      let passedPolyline = new AMap.Polyline({
+        map: this.mapObj,
+        // path: lineArr,
+        strokeColor: "#AF5",  //线颜色
+        // strokeOpacity: 1,     //线透明度
+        strokeWeight: 6,      //线宽
+        // strokeStyle: "solid"  //线样式
+      });//小车跑起来的路线
+      //on函数的作用  监听他的状态
+      this.car_marker.on('moving', function (e) {
+        passedPolyline.setPath(e.passedPath);
+      });
+      //根据地图上添加的覆盖物分布情况，自动缩放地图到合适的视野级别，参数均可缺省。
+      this.mapObj.setFitView();
+      this.car_marker.moveAlong(this.lines.find(item => item.id == val).lineArr, 2000);
+      //函数执行完了以后应该把小车移走
+      // this.mapObj.remove(marker);
     }
   },
   //初始化地图
   async created() {
-    console.log(this.lngLats.length+"+");
     // 已载入高德地图API，则直接初始化地图
     if (window.AMap && window.AMapUI) {
-      console.log(this.lngLats.length+"!");
       this.initMap();
       // 未载入高德地图API，则先载入API再初始化
     } else {
-      console.log(this.lngLats.length+"...");
       //官网有说采用异步的方式加载高德地图
       await remoteLoad(`http://webapi.amap.com/maps?v=1.3&key=${MapKey}`)
       await remoteLoad('http://webapi.amap.com/ui/1.0/main.js')
+      //以上两个加载完了再初始化地图
       this.initMap();
     }
   }
